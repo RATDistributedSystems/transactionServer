9142d3ac004daf1c38e2066a0ad560db6c7dd424
Rafael Solorzano <rafa@solorzanoc.com>
2018-01-24 23:51:37 -0800

    add all log functions

--- quoteServerTest.go
+++ quoteServerTest.go
@@ -223,6 +223,30 @@ func logQuoteEvent(time string, server string, transactionNum string, price stri
 	fmt.Fprintf(conn,text + "\n") 
 }
 
+func logSystemEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string){
+	conn, _ := net.Dial("tcp", "192.168.3.102:5555")
+	text := "System" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds
+	fmt.Fprintf(conn,text + "\n") 
+}
+
+func logAccountTransactionEvent(time string, server string, transactionNum string, action string, userid string, funds string){
+	conn, _ := net.Dial("tcp", "192.168.3.102:5555")
+	text := "Account" + "," + time + "," + server + "," + transactionNum + "," + action + "," + userid + "," + funds
+	fmt.Fprintf(conn,text + "\n") 	
+}
+
+func logErrorEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string, error string){
+	conn, _ := net.Dial("tcp", "192.168.3.102:5555")
+	text := "User" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds + "," + error
+	fmt.Fprintf(conn,text + "\n") 
+}
+
+func logDebugEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string, debug string){
+	conn, _ := net.Dial("tcp", "192.168.3.102:5555")
+	text := "User" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds + "," + debug
+	fmt.Fprintf(conn,text + "\n") 
+}
+
 func dumpUser(userId string){
 	fmt.Println("In Dump user")
 	conn, _ := net.Dial("tcp", "192.168.3.102:5555")
