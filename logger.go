b1c1aafb8faac38990c6a86b4bfcdea0b560a4e7
asinha94 <asinha94@gmail.com>
2018-02-01 02:42:13 -0800

    refactored logging to audit server

--- logger.go
+++ logger.go
@@ -1,55 +1,47 @@
 package main
 
-import "net"
-import "fmt"
-//import "bufio"
-//import "os"
-//import "github.com/gocql/gocql"
-//import "strconv"
-//import "strings"
-//import "github.com/go-redis/redis"
-//import "github.com/twinj/uuid"
-//import "time"
-//import "log"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
-
-
-func logUserEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string){
-	//connect to audit server
-	conn, _ := net.Dial("tcp", "localhost:44445")
-	text := "User" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds
-	fmt.Fprintf(conn,text + "\n")
-	//go logSystemEvent(time, "AU1", "1",command,userid,"","")
+func sendMsgToAuditServer(msg string) {
+	addr, protocol := configurationServer.GetServerDetails("audit")
+	conn, err := net.Dial(protocol, addr)
+	if err != nil {
+		log.Fatalf("Couldn't Connect to Audit server: " + err.Error())
+	}
+	defer conn.Close()
+	fmt.Fprintln(conn, msg)
 }
 
-func logQuoteEvent(time string, server string, transactionNum string, price string, stockSymbol string, userid string, quoteservertime string, cryptokey string){
-	conn, _ := net.Dial("tcp", "localhost:44445")
-	text := "Quote" + "," + time + "," + server + "," + transactionNum + "," + price + "," + stockSymbol + "," + userid + "," + quoteservertime + "," + cryptokey
-	fmt.Fprintf(conn,text + "\n") 
+func logUserEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string) {
+	msg := fmt.Sprintf("User, %s, %s, %s, %s, %s, %s, %s", time, server, transactionNum, command, userid, stockSymbol, funds)
+	sendMsgToAuditServer(msg)
 }
 
-func logSystemEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string){
-	conn, _ := net.Dial("tcp", "localhost:44445")
-	text := "System" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds
-	fmt.Fprintf(conn,text + "\n") 
+func logQuoteEvent(time string, server string, transactionNum string, price string, stockSymbol string, userid string, quoteservertime string, cryptokey string) {
+	msg := fmt.Sprintf("Quote, %s, %s, %s, %s, %s, %s, %s, %s", time, server, transactionNum, price, stockSymbol, userid, quoteservertime, cryptokey)
+	sendMsgToAuditServer(msg)
 }
 
-func logAccountTransactionEvent(time string, server string, transactionNum string, action string, userid string, funds string){
-	conn, _ := net.Dial("tcp", "localhost:44445")
-	text := "Account" + "," + time + "," + server + "," + transactionNum + "," + action + "," + userid + "," + funds
-	fmt.Fprintf(conn,text + "\n") 	
+func logSystemEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string) {
+	msg := fmt.Sprintf("System, %s, %s, %s, %s, %s, %s, %s", time, server, transactionNum, command, userid, stockSymbol, funds)
+	sendMsgToAuditServer(msg)
 }
 
-func logErrorEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string, error string){
-	conn, _ := net.Dial("tcp", "localhost:44445")
-	text := "User" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds + "," + error
-	fmt.Fprintf(conn,text + "\n") 
+func logAccountTransactionEvent(time string, server string, transactionNum string, action string, userid string, funds string) {
+	msg := fmt.Sprintf("Account, %s, %s, %s, %s, %s, %s", time, server, transactionNum, action, userid, funds)
+	sendMsgToAuditServer(msg)
 }
 
-func logDebugEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string, debug string){
-	conn, _ := net.Dial("tcp", "localhost:44445")
-	text := "User" + "," + time + "," + server + "," + transactionNum + "," + command + "," + userid + "," + stockSymbol + "," + funds + "," + debug
-	fmt.Fprintf(conn,text + "\n") 
+func logErrorEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string, err string) {
+	msg := fmt.Sprintf("User, %s, %s, %s, %s, %s, %s, %s, %s", time, server, transactionNum, command, userid, stockSymbol, funds, err)
+	sendMsgToAuditServer(msg)
 }
 
-
+func logDebugEvent(time string, server string, transactionNum string, command string, userid string, stockSymbol string, funds string, debug string) {
+	msg := fmt.Sprintf("User, %s, %s, %s, %s, %s, %s, %s, %s", time, server, transactionNum, command, userid, stockSymbol, funds, debug)
+	sendMsgToAuditServer(msg)
+}
