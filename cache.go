d3627b4553cb5e4c7e49c9268ec094534cfc48ac
tkun3 <tapimlott@live.ca>
2018-02-22 11:45:24 -0800

    cache checker

--- cache.go
+++ cache.go
@@ -2,20 +2,59 @@ package main
 
 import (
 	//"bufio"
-	//"fmt"
+	"fmt"
+	"time"
 	"strings"
+	"github.com/go-redis/redis"
 )
 
+var redisConn *redis.Client
+
+const ttl = time.Second * 60
+
+
+func initRedis(){
+	redisConn = redis.NewClient(&redis.Options{
+		Network:  "tcp",
+		Addr:     "localhost:6379",
+		Password: "", 
+		DB:       0, //using the default DB
+	})
+	fmt.Println("Redis Connection Created")
+}
+
+func cacheAdd(stockName string, stockInfo string){
+	fmt.Printf("Adding %s to cache \n", stockName)
+	err := redisConn.Set(stockName, stockInfo, ttl).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func cacheExists(stock string) bool{
-	return true
+	fmt.Printf("checking if %s exists", stock)
+	val, err := redisConn.Get(stock).Result()
+	fmt.Println(val)
+	if err == redis.Nil{
+		fmt.Printf("Cache %s DNE \n", stock)
+		return false
+	}else if err != nil {
+		panic(err)
+	}else {
+		fmt.Printf("Cache %s Exists \n", stock)
+		return true
+	}
 
 }
 
 func cacheReturn(stock string) []string {
-	//query the db to get the data
-	message := "quote, here"
+	fmt.Printf("Returning Cache Key %s \n", stock)
+	val, err := redisConn.Get(stock).Result()
+	if err != nil{
+		panic(err)
+	}
 	//break comma delimitted data in to a message
-	messageArray := strings.Split(message, ",")
+	messageArray := strings.Split(val, ",")
 	//return the quote array
 	return messageArray
 }
--- command_quoteCache.go
+++ command_quoteCache.go
@@ -12,14 +12,17 @@ func quoteCacheRequest(userId string, stockSymbol string, transactionNum int) []
 
 	//check if a cached quote exists
 	if(cacheExists(stockSymbol) == true){
+		fmt.Println("check if key exists")
 		//obtain values 
 		return cacheReturn(stockSymbol)
 	}else{
 		//if it doesnt access the quote server normally
+		logUserEvent("TS1", transactionNum, "QUOTE", userId, stockSymbol, "")
 		conn := GetQuoteServerConnection() //conn := quotePool.getConnection()
-		fmt.Fprintf(conn, "%s,%s", stockSymbol, userId)
+		fmt.Fprintf(conn, "%s,%s \n", stockSymbol, userId)
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		conn.Close() //quotePool.returnConnection(conn)
+		cacheAdd(stockSymbol, message)
 		messageArray := strings.Split(message, ",")
 		logQuoteEvent("TS1", transactionNum, messageArray[0], messageArray[1], messageArray[2], messageArray[3], messageArray[4])
 		return messageArray
