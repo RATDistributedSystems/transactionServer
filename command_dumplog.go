c85d5bdda330b4262a012ddab193c89deb91a8fa
asinha94 <asinha94@gmail.com>
2018-03-31 14:57:07 -0700

    replaced map with conc map for better thread safety. updated dumplog to handle both dumplog and dump user

--- command_dumplog.go
+++ command_dumplog.go
@@ -7,26 +7,22 @@ import (
 
 type commandDumplog struct {
 	username string
-}
-
-func (c commandDumplog) process(transaction int) {
-	logUserEvent(serverName, transaction, "DUMPLOG", "-1", "", "")
-	dump(c.username, transaction)
-}
-
-type commandDumplogUsername struct {
-	username string
 	filename string
 }
 
-func (c commandDumplogUsername) process(transaction int) {
+func (c commandDumplog) process(transaction int) {
 	logUserEvent(serverName, transaction, "DUMPLOG", c.username, "", "")
-	dumpUser(c.username, c.filename, transaction)
+	if c.username == "-1" {
+		dump(c.username, transaction)
+	} else {
+		dumpUser(c.username, c.filename, transaction)
+	}
+
 }
 
-func dumpUser(userId string, filename string, transactionNum int) {
-	sendMsgToAuditServer(fmt.Sprintf("DUMPLOG,%s,%s", userId, filename))
-	log.Printf("Dumping log data for %s\n", userId)
+func dumpUser(userID string, filename string, transactionNum int) {
+	sendMsgToAuditServer(fmt.Sprintf("DUMPLOG,%s,%s", userID, filename))
+	log.Printf("Dumping log data for %s\n", userID)
 }
 
 func dump(filename string, transactionNum int) {
