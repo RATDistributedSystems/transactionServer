34d1246c6c8c65f2d2f07cae1ccdf317bed646b4
tkun3 <tapimlott@live.ca>
2018-04-01 19:04:14 -0700

    readded trigger stuff

--- command_sellTrigger.go
+++ command_sellTrigger.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-
+	"time"
 	"github.com/RATDistributedSystems/utilities/ratdatabase"
 )
 
@@ -92,7 +92,7 @@ func setSellTrigger(userID string, stock string, stockSellPrice string, transact
 		logErrorEvent(serverName, transactionNum, "SET_SELL_TRIGGER", userID, stock, stockSellPrice, m)
 		return m
 	}
-
+	checkSellTrigger(userID, stock, stockValueCents, transactionNum)
 	return fmt.Sprintf("Successfully set SET_SELL_TRIGGER (%s) for %s", stockSellPrice, stock)
 }
 
@@ -110,4 +110,39 @@ func cancelSellTrigger(userID string, stock string, transactionNum int) string {
 	ratdatabase.UpdateUserStockByUserAndStock(userID, stock, newStockAmount)
 
 	return fmt.Sprintf("Cancelled trigger for %s", stock)
+}
+
+func checkSellTrigger(userId string, stock string, stockSellPriceCents int, transactionNum int) {
+	operation := false
+	for {
+		//check the quote server every 5 seconds
+		timer1 := time.NewTimer(time.Second * 1)
+		<-timer1.C
+		//if the trigger doesnt exist exit
+		exists := checkTriggerExists(userId, stock, operation)
+		if exists == false {
+			return
+		}
+		//retrieve current stock price
+		currentStockPrice := getQuote(userId, stock, transactionNum)
+		if currentStockPrice > stockSellPriceCents {
+			//sell the allocated stocks
+			//get stocks allocated to sell
+			var pendingStocks int
+			if err := sessionGlobal.Query("SELECT stockAmount FROM sellTriggers WHERE userid='" + userId + "' AND stock='" + stock + "' ").Scan(&pendingStocks); err != nil {
+				//panic(fmt.Sprintf("Problem inputting to Triggers Table", err))
+				return
+			}
+			sellProfits := pendingStocks * currentStockPrice
+			//delete pending transaction
+			if err := sessionGlobal.Query("DELETE FROM sellTriggers WHERE userid='" + userId + "' AND stock='" + stock + "' ").Exec(); err != nil {
+				//panic(err)
+				return
+			}
+			//add profits from selling stock to account
+			fmt.Println("Sell Trigger Sucessful, profits added to account")
+			addFunds(userId, sellProfits)
+			return
+		}
+	}
 }
