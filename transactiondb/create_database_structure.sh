519e9100f431db5938d46cd45622d12f02277154
asinha94 <asinha94@gmail.com>
2018-04-02 20:41:26 -0700

    increased timeout

--- transactiondb/create_database_structure.sh
+++ transactiondb/create_database_structure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mv cassandra.yaml /etc/cassandra/
 (sleep 65 && cqlsh -f create_tsdb_structure.cql) &
 cd / && docker-entrypoint.sh
---
+++ transactiondb/ratslavedb/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv cassandra.yaml /etc/cassandra
+
+/docker-entrypoint.sh
